fe3c909fcff8c60a19af3e6bb4c6cc8d68f7103f
annatrajano <annatrajano95@gmail.com@example.com>
2023-08-26 16:43:16 -0300

    feat: :sparkles: add react query to routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,24 @@
 // Main imports
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 // Pages
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 
+// React Query config
+const queryClient = new QueryClient();
+
 function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
