ba5003d432edcbdaaf89be4b50027a3537e27344
annatrajano <annatrajano95@gmail.com@example.com>
2023-08-30 08:34:27 -0300

    feat: :sparkles: add header component with styles

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+// Main import
+import React from "react";
+import { BiFoodMenu } from "react-icons/bi";
+
+//  Styles - Header
+import styles from "./Header.module.css";
+
+export default function Header({ userName }) {
+  return (
+    <header className={styles.header__container}>
+      <h1 className={styles.title__home}>
+        My Recipes <BiFoodMenu />{" "}
+      </h1>
+      <h2 className={styles.username__text}>{userName}</h2>
+    </header>
+  );
+}
