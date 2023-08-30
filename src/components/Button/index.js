fab2630c1f0f94fe5306a49b5132f75c5d61bab7
annatrajano <annatrajano95@gmail.com@example.com>
2023-08-30 08:33:26 -0300

    feat: :sparkles: add button component with style

---
+++ src/components/Button/index.js
@@ -0,0 +1,18 @@
+// Main import
+import React from "react";
+
+// Styles - Button
+import styles from "./Button.module.css"
+
+export default function Button({text, onClick, icon}) {
+  return (
+    <button
+     className={styles.bttn__category}
+      type="button"
+      onClick={() => onClick(text)}
+    >
+      <p>{icon}</p>
+      <p className={styles.text_icon__category}>{text}</p>
+    </button>
+  );
+}
