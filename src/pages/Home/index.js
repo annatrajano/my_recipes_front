92ea8f03bebb9a1803aeaaccacc3dcc671da0c98
annatrajano <annatrajano95@gmail.com@example.com>
2023-08-30 08:36:25 -0300

    feat: :sparkles: add home page style and logic

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,69 @@
-import React from 'react'
+// Main import
+import React from "react";
+import { GiCirclingFish, GiHotMeal, GiCupcake, GiGreenhouse } from "react-icons/gi";
+import { LuBeef, LuVegan } from "react-icons/lu";
+import {MdMenuBook} from "react-icons/md"
+
+// Components
+import Header from "../../components/Header";
+import Button from "../../components/Button";
+
+// Styles - Home
+import styles from "./Home.module.css";
 
 export default function Home() {
+  function handleClick(category) {
+    console.log(category);
+  }
+
+  const buttons = [
+    {
+      text: "Seafood",
+      onclik: handleClick,
+      icon: <GiCirclingFish />,
+    },
+    {
+      text: "Beef",
+      onclik: handleClick,
+      icon: <LuBeef />,
+    },
+    {
+      text: "Pasta",
+      onclik: handleClick,
+      icon: <GiHotMeal />,
+    },
+    {
+      text: "Vegan",
+      onclik: handleClick,
+      icon: <LuVegan/>,
+    },
+    {
+      text: "Vegetarian",
+      onclik: handleClick,
+      icon: <GiGreenhouse/>,
+    },
+    {
+      text: "Dessert",
+      onclik: handleClick,
+      icon: <GiCupcake />,
+    },
+    {
+      text: "My recipes",
+      onclik: handleClick,
+      icon: <MdMenuBook />,
+    },
+  ];
+
   return (
-    <div>Home</div>
-  )
+    <>
+      <Header userName={"annaBia"} />
+      <section className={styles.section__container}>
+        {buttons.map((item) => {
+          return (
+            <Button text={item.text} onClick={item.onclik} icon={item.icon} />
+          );
+        })}
+      </section>
+    </>
+  );
 }
