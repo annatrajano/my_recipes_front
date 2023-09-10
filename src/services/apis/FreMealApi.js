db26dde80f290bc38a25ed6fe534985415f912be
annatrajano <annatrajano95@gmail.com@example.com>
2023-09-10 20:34:13 -0300

    refactor: :art: improving freeMealApi - services

--- src/services/apis/FreMealApi.js
+++ src/services/apis/FreMealApi.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 //Define API BASE_URL
-const { REACT_APP_API_MEAL_DB } = "www.themealdb.com/api/json/v1/1/"; 
+const  REACT_APP_API_MEAL_DB = "https://www.themealdb.com/api/json/v1/1/"; 
 
 // Create axios instance, with specif request props(headers, auth, body, etc...)
 export default axios.create({
-    baseURL: 'https://www.themealdb.com/api/json/v1/1/',
+    baseURL: REACT_APP_API_MEAL_DB,
 });
