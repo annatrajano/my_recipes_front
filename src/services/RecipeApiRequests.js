f044986dbb5afb0383a60e55140f55764d3f00b6
annatrajano <annatrajano95@gmail.com@example.com>
2023-09-15 16:38:25 -0300

    feat: :sparkles: implements recipe api requests

---
+++ src/services/RecipeApiRequests.js
@@ -0,0 +1,38 @@
+// Import axios from specific API definition
+import axios from "./apis/RecipesApi"
+
+
+// GET Method Request for Recipes
+async function getAllRecipes() {
+  try {
+    const response = await axios.get(`/recipes`);
+    return response.data.recipes;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+// POST Method Request for Recipe
+async function createRecipe(data) {
+  try {
+    const response = await axios.post(
+      `/recipe`,
+      JSON.stringify(data),
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+// DELETE Method Request by Recipe Id
+async function deleteRecipeById(id) {
+  try {
+    const response = await axios.delete(`/recipe/${id}/`);
+    return response.data;
+  } catch (error) {
+    console.log("Não foi possivel deletar esse caso!");
+  }
+}
+
+export {getAllRecipes, createRecipe, deleteRecipeById}
---
+++ src/services/apis/RecipesApi.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+//Define API BASE_URL
+const  REACT_APP_API_RECIPE_DB = "http://127.0.0.1:5000"; 
+
+// Create axios instance, with specif request props(headers, auth, body, etc...)
+export default axios.create({
+    baseURL: REACT_APP_API_RECIPE_DB,
+});
